33ebef906ce5d0ec8be7a641762d652816764c89
Richard Kersey <github@razorit.com>
2018-05-22 04:32:23 +0000

    added initial controllers

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $accounts = Account::all();
+        return response()->json($accounts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $account = new Account([
+            'title' => $request->get('title'),
+            'body' => $request->get('body')
+        ]);
+        $account->save();
+
+
+        return response()->json('Account Added Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $account = Account::find($id);
+        return response()->json($account);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $account = Account::find($id);
+        $account->title = $request->get('title');
+        $account->body = $request->get('body');
+        $account->save();
+
+
+        return response()->json('Account Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $account = Account::find($id);
+        $account->delete();
+
+
+        return response()->json('Account Deleted Successfully.');
+    }
+}

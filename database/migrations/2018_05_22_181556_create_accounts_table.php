b15f96a060c66cd7a8feae675df30dd25fc8373f
Richard Kersey <github@razorit.com>
2018-05-23 19:20:49 +0000

    saving actually works now

--- database/migrations/2018_05_22_181556_create_accounts_table.php
+++ database/migrations/2018_05_22_181556_create_accounts_table.php
@@ -14,7 +14,7 @@ class CreateAccountsTable extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->increments('id');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
